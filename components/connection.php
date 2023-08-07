48bd8491f17ca032a44d24bbf54ea40e13898d82
Nimra <nimrasattar6050gmail.com>
2023-08-07 10:00:17 +0500

    completed first hour

---
+++ components/connection.php
@@ -0,0 +1,19 @@
+<?php
+    $db_name =' mysql:host=localhost;dbname=shop_db';
+    $db_user = 'root';
+    $db_password = '';
+
+    $conn = new POO($db_name,$db_user,$db_password);
+    if($conn){
+        echo = "connected";
+    }
+    function unique_id(){
+        $chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charLength = strlen($chars);
+        $randomstring = '';
+        for($i=0; $i < 20; $i++);{
+            $randomstring = $chars[mt_rand(0, $charlength - 1)];
+    }
+    return $randomstring;
+}
+?>
